3e311ae858cdf721a1e6cccbc1955c420a45011f
JAYESHTAPDIYA12 <jayesh121220@gmail.com>
2025-09-23 21:57:05 +0530

    [jayesh tapdiya] added the routing for the single page book

--- bookstore/src/routes/Router.tsx
+++ bookstore/src/routes/Router.tsx
@@ -4,6 +4,7 @@ import Login from "../pages/Login";
 import Dashboard from "../pages/Dashboard";
 import { AuthRoute } from "./authRouts";
 import { ProtectedRoute } from "./protectedRoute";
+import SinglePageBook from "../pages/SinglePageBook";
 const Router: React.FC = () => {
   return (
     <>
@@ -13,11 +14,29 @@ const Router: React.FC = () => {
             path="/"
             element={
               <ProtectedRoute>
-                <Dashboard />{" "}
+                <Dashboard />
               </ProtectedRoute>
             }
           />
 
+          <Route
+            path="/book/:id"
+            element={
+              <ProtectedRoute>
+                <SinglePageBook />
+              </ProtectedRoute>
+            }
+          />
+          {/* <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          >
+            <Route path="book/:id" element={<SinglePageBook />} />
+          </Route> */}
           <Route
             path="/login"
             element={
