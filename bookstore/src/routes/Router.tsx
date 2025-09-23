1bc5c79f5e8d0d3fe50751eb2bb3925f5cd46772
JAYESHTAPDIYA12 <jayesh121220@gmail.com>
2025-09-23 12:36:00 +0530

    [jayesh tapdiya] added the routing for the dashboard.ts

--- bookstore/src/routes/Router.tsx
+++ bookstore/src/routes/Router.tsx
@@ -1,12 +1,31 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "../pages/Login";
+import Dashboard from "../pages/Dashboard";
+import { AuthRoute } from "./authRouts";
+import { ProtectedRoute } from "./protectedRoute";
 const Router: React.FC = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Login />} />
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <Dashboard />{" "}
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/login"
+            element={
+              <AuthRoute>
+                <Login />
+              </AuthRoute>
+            }
+          />
         </Routes>
       </BrowserRouter>
     </>
