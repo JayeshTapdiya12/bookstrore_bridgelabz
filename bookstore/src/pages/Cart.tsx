91640695f04e10bbf6f2bbd115771bc969cfed1d
JAYESHTAPDIYA12 <jayesh121220@gmail.com>
2025-09-26 15:03:03 +0530

    [jayesh tapdiya] updated the cart page for adding the address and the order summary

--- bookstore/src/pages/Cart.tsx
+++ bookstore/src/pages/Cart.tsx
@@ -1,41 +1,61 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import {
   getCart as getCartService,
-  updateQuant as updateQuantityService,
+  addCart as updateQuantityService,
+  rmeoveCart as RemoveCartService,
 } from "../service/cartService";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
 import { CircularProgress } from "@mui/material";
 import "../style/cart.css";
-
+import { QuntContext } from "../pages/Dashboard";
+import bookImmg from "../assets/Bookimg2.png";
+import AddressForm from "./AddressForm";
+import { addOrder as AddOrderService } from "../service/orderService";
+import { useNavigate } from "react-router-dom";
 interface Book {
-  _id: string; // cart item id
-  quantityToBuy: number;
-  product_id: {
-    _id: string; // book id
-    bookName: string;
-    author: string;
-    description: string;
-    quantity: number;
-    price: number;
-    discountPrice: number;
-    bookImage: string;
+  code: number;
+  message: string;
+  data: {
+    data: {
+      isPurchased: boolean;
+      _id: string;
+      cartBY: string;
+      book: {
+        _id: string;
+        description: string;
+        discountPrice: number;
+        bookName: string;
+        author: string;
+        quantity: number;
+        price: number;
+      }[];
+      cartTotal: number;
+      createdAt: string;
+      updatedAt: string;
+      __v: number;
+    };
   };
 }
 
 const Cart: React.FC = () => {
   const [cart, setCart] = useState<Book[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
+  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen2, setIsOpen2] = useState(false);
+
+  const [totalPrice, setTotalPrice] = useState<number>(0);
+  const { refreshCart } = useContext(QuntContext);
+
+  const navigate = useNavigate();
 
   const cartGet = async () => {
     try {
       const res = await getCartService();
-      const bookcart = res?.data?.result;
-      if (Array.isArray(bookcart)) {
-        setCart(bookcart);
-      } else {
-        setCart([]);
-      }
+      const bookcart = res?.data?.data?.book;
+      console.log(res?.data?.data?.book);
+      setTotalPrice(res?.data?.data?.cartTotal);
+      setCart(bookcart);
     } catch (error) {
       console.error("Error fetching cart:", error);
       setCart([]);
@@ -54,88 +74,131 @@ const Cart: React.FC = () => {
   ) => {
     setCart((prevCart) =>
       prevCart.map((book) => {
-        if (book.product_id._id === bookId) {
-          const newQuantity =
-            action === "increment"
-              ? book.quantityToBuy + 1
-              : Math.max(1, book.quantityToBuy - 1);
-
-          updateQuantityService(bookId, newQuantity);
-
-          return { ...book, quantityToBuy: newQuantity };
+        if (book._id === bookId) {
+          if (action === "increment") {
+            updateQuantityService(bookId);
+          } else if (action === "decrement") {
+            RemoveCartService(bookId);
+          }
+          cartGet();
+          refreshCart();
         }
         return book;
       })
     );
   };
 
-  // Handle remove
-  const handleRemove = (bookId: string) => {
-    setCart((prevCart) =>
-      prevCart.filter((book) => book.product_id._id !== bookId)
-    );
+  const checkout = async () => {
+    const res = await AddOrderService();
+    console.log(res);
+    navigate("/order");
   };
 
   return (
-    <div className="cart-container">
-      {loading ? (
-        <Stack spacing={2}>
-          <Stack direction="row" spacing={0}>
-            <Button variant="outlined" disabled>
-              <CircularProgress size={24} />
-            </Button>
+    <>
+      <div className="cart-container">
+        <h4 style={{ color: "grey" }}>
+          Home/
+          <span style={{ color: "black", fontSize: "15px" }}> (Cart)</span>
+        </h4>
+        {loading ? (
+          <Stack spacing={2}>
+            <Stack direction="row" spacing={0}>
+              <Button variant="outlined" disabled>
+                <CircularProgress size={10} />
+              </Button>
+            </Stack>
           </Stack>
-        </Stack>
-      ) : cart.length === 0 ? (
-        <p>Your cart is empty</p>
-      ) : (
-        cart.map((ele) => (
-          <div key={ele._id} className="cart-item">
-            <img
-              src={ele.product_id.bookImage}
-              alt={ele.product_id.bookName}
-              className="book-image"
-            />
-            <div className="cart-details">
-              <h3>Book Name: {ele.product_id.bookName}</h3>
-              <p>Author: {ele.product_id.author}</p>
+        ) : cart?.length === 0 ? (
+          <p>Your cart is empty</p>
+        ) : (
+          cart?.map((ele) => (
+            <div key={ele._id} className="cart-item">
+              <img
+                src={!ele.bookImage ? bookImmg : ele.bookImage}
+                alt={ele.bookName}
+                className="book-image"
+              />
+              <div className="cart-details">
+                <h3>Book Name: {ele.bookName}</h3>
+                <p>Author: {ele.author}</p>
 
-              <div className="cart-price">
-                <span>Rs. {ele.product_id.discountPrice}</span>
-                <span className="cart-original-price">
-                  Rs. {ele.product_id.price}
-                </span>
+                <div className="cart-price">
+                  <span>Rs. {ele.discountPrice}</span>
+                  <span className="cart-original-price">Rs. {ele.price}</span>
+                </div>
+
+                <div className="cart-price">
+                  total item price:{" "}
+                  <span>Rs. {ele.discountPrice * ele.quantity}</span>
+                </div>
+
+                <div className="cart-quantity">
+                  <button
+                    onClick={() => handleQuantityChange(ele._id, "decrement")}
+                  >
+                    -
+                  </button>
+                  <input type="text" value={ele.quantity} readOnly />
+                  <button
+                    onClick={() => handleQuantityChange(ele._id, "increment")}
+                  >
+                    +
+                  </button>
+                </div>
               </div>
-
-              <div className="cart-quantity">
-                <button
-                  onClick={() =>
-                    handleQuantityChange(ele.product_id._id, "decrement")
-                  }
-                >
-                  -
-                </button>
-                <input type="text" value={ele.quantityToBuy} readOnly />
-                <button
-                  onClick={() =>
-                    handleQuantityChange(ele.product_id._id, "increment")
-                  }
-                >
-                  +
-                </button>
-              </div>
-
-              <button
-                className="remove-btn"
-                onClick={() => handleRemove(ele.product_id._id)}
-              >
-                Remove
-              </button>
             </div>
-          </div>
-        ))
-      )}
-    </div>
+          ))
+        )}
+        <div className="cart-price">
+          <h4>
+            Total Cart Price : <span>Rs. {totalPrice}</span>
+          </h4>
+        </div>
+      </div>
+
+      {/* address details */}
+
+      <div onClick={() => setIsOpen(!isOpen)} className="cart-container">
+        {isOpen ? "Address Details" : "Address Details"}
+      </div>
+
+      {isOpen && <AddressForm />}
+
+      <div onClick={() => setIsOpen2(!isOpen2)} className="cart-container">
+        {isOpen2 ? "Close Order Details" : "Order Details"}
+        {isOpen2 && (
+          <>
+            {cart.map((ele) => (
+              <div key={ele._id} className="cart-item">
+                <img
+                  src={!ele.bookImage ? bookImmg : ele.bookImage}
+                  alt={ele.bookName || "Book Image"}
+                  className="book-image"
+                />
+                <div className="cart-details">
+                  <h3>Book Name: {ele.bookName}</h3>
+                  <p>Author: {ele.author}</p>
+
+                  <div className="cart-price">
+                    <span>Rs. {ele.discountPrice}</span>
+                    <span className="cart-original-price">Rs. {ele.price}</span>
+                  </div>
+
+                  <div className="cart-price">
+                    Total item price:{" "}
+                    <span>Rs. {ele.discountPrice * ele.quantity}</span>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </>
+        )}
+        <Button variant="contained" color="primary" onClick={checkout}>
+          checkout
+        </Button>
+      </div>
+    </>
   );
 };
 
