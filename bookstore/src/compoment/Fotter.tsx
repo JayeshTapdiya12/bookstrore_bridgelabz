d619a05f2c6e04ff51524f280424df145272b980
JAYESHTAPDIYA12 <jayesh121220@gmail.com>
2025-09-26 14:55:40 +0530

    [jayesh tapdiya] added the footer file

---
+++ bookstore/src/compoment/Fotter.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../style/fotter.css";
+const Fotter: React.FC = () => {
+  return (
+    <>
+      <br />
+      <footer>
+        Copyright © 2020, Bookstore Private Limited. All Rights Reserved
+      </footer>
+    </>
+  );
+};
+
+export default Fotter;
