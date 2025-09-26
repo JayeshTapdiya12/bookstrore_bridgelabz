436e62a39b4aa8b94884169194bca65c0a90ffc2
JAYESHTAPDIYA12 <jayesh121220@gmail.com>
2025-09-26 14:57:24 +0530

    [jayesh tapdiya] updated  the  book service and added the authentication

--- bookstore/src/service/bookSerivce.ts
+++ bookstore/src/service/bookSerivce.ts
@@ -1,7 +1,8 @@
 import axios, { type AxiosResponse } from "axios";
 
-const baseUrl =
-  "https://bookstore.incubation.bridgelabz.com/bookstore_user/get/book";
+const baseUrl = "http://localhost:3001/api/v1/books";
+const token = localStorage.getItem("token");
+const headers = { headers: { Authorization: "bearer " + token } };
 
 interface bookResp {
   success: boolean;
@@ -23,6 +24,15 @@ interface bookResp {
 }
 
 export const getBook = async (): Promise<AxiosResponse<bookResp>> => {
-  let res = await axios.get(baseUrl);
+  let res = await axios.get(baseUrl, headers);
+  console.log(res);
+  return res;
+};
+
+export const getBookById = async (
+  id: string
+): Promise<AxiosResponse<bookResp>> => {
+  let res = await axios.get(`${baseUrl}/${id}`, headers);
+  console.log(res);
   return res;
 };
