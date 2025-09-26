d1fd182d66e96bb7bbf1d2dcbe78159a9746ff9e
JAYESHTAPDIYA12 <jayesh121220@gmail.com>
2025-09-26 14:57:40 +0530

    [jayesh tapdiya] added the cart service file

--- bookstore/src/service/cartService.ts
+++ bookstore/src/service/cartService.ts
@@ -1,92 +1,52 @@
 import axios, { type AxiosResponse } from "axios";
 
-const baseUrl = "https://bookstore.incubation.bridgelabz.com/bookstore_user";
+const baseUrl = "http://localhost:3001/api/v1/books/cart";
 
 const token = localStorage.getItem("token");
-// const headers = { headers: { Authorization: "bearer " + token } };
+const headers = { headers: { Authorization: "bearer " + token } };
 
-const headers = { headers: { "x-access-token": token } };
 interface GetCart {
-  success: string;
-  messgae: string;
-  result: [
-    {
-      product_id: {
-        description: string;
-        discountPrice: number;
-        bookImage: string | any;
-        _id: string;
-        admin_user_id: string;
-        bookName: string;
-        author: string;
-        quantity: number;
-        price: number;
-        createdAt: string;
-        updatedAt: string;
-        __v: number;
-      };
-      quantityToBuy: number;
-      _id: string;
-      user_id: {
-        address: Array<any>;
-        isVerified: boolean;
-        _id: string;
-        fullName: string;
-        email: string;
-        phone: string;
-        createdAt: string;
-        updatedAt: string;
-        __v: number;
-      };
-    }
-  ];
-}
-
-export const getCart = async (): Promise<AxiosResponse<GetCart>> => {
-  const res = await axios.get(`${baseUrl}/get_cart_items`, headers);
-  console.log(res);
-  return res;
-};
-
-//
-// {
-//   "success": true,
-//   "message": "Item added to cart",
-//   "result": {
-//     "product_id": "66d5564ed84c6c000e3b36a9",
-//     "quantityToBuy": 1,
-//     "_id": "68d2c22156bee9000eba9333",
-//     "user_id": "68d11db8b9bfd1000e248cc0",
-//     "createdAt": "2025-09-23T15:52:01.750Z",
-//     "updatedAt": "2025-09-23T15:52:01.750Z",
-//     "__v": 0
-//   }
-
-interface AddCart {
-  success: boolean;
+  code: number;
   message: string;
-
-  result: {
-    product_id: string;
-    quantityToBuy: number;
+  data: {
+    isPurchased: boolean;
     _id: string;
-    user_id: string;
+    cartBY: string;
+    book: {
+      _id: string;
+      description: string;
+      discountPrice: number;
+      bookName: string;
+      author: string;
+      quantity: number;
+      price: number;
+    }[];
+    cartTotal: number;
     createdAt: string;
     updatedAt: string;
     __v: number;
   };
 }
-export const addCart = async (id: string): Promise<AxiosResponse<AddCart>> => {
-  const res = await axios.post(`${baseUrl}/add_cart_item/${id}`, {}, headers);
-  console.log(res);
+
+export const getCart = async (): Promise<AxiosResponse<GetCart>> => {
+  const res = await axios.get(`${baseUrl}/getcart`, headers);
   return res;
 };
 
-// update the cart  item:/cart_item_quantity/{cartItem_id}
+interface AddCart {
+  code: number;
+  message: string;
+  data: GetCart["data"];
+}
 
-// {
-//   "success": true,
-//   "message": "Cart item quantity updated"
-// }
+export const addCart = async (id: string): Promise<AxiosResponse<AddCart>> => {
+  const res = await axios.post(`${baseUrl}/add/${id}`, {}, headers);
+  return res;
+};
 
-// remove item from cart- remove_cart_item/66d55482d84c6c000e3b369e
+export const rmeoveCart = async (
+  id: string
+): Promise<AxiosResponse<AddCart>> => {
+  const res = await axios.delete(`${baseUrl}/remove/${id}`, headers);
+  return res;
+};
