e1bc33d35be730858349f93bd7a4ec19153be40b
JAYESHTAPDIYA12 <jayesh121220@gmail.com>
2025-09-29 12:37:56 +0530

    [jayesh tapdiya]added the wishlist and aslo added the remove wishlist functionlity

---
+++ bookstore/src/pages/Wishlist.tsx
@@ -0,0 +1,105 @@
+import React, { useEffect, useState } from "react";
+import { getWishList as getWishListService } from "../service/wishlistService";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import { Grid } from "@mui/material";
+import { CircularProgress } from "@mui/material";
+import bookImmg from "../assets/Bookimg2.png";
+import { styled } from "@mui/system";
+import { removeWishlist as removeWislListService } from "../service/wishlistService";
+
+interface Book {
+  code: number;
+  message: string;
+  wishlist: null | {
+    _id: string;
+    wishBy: string;
+    book: [
+      {
+        _id: string;
+        bookId: string;
+        bookname: string;
+        authorname: string;
+        image: string;
+      }
+    ];
+    createdAt: string;
+    updatedAt: string;
+    __v: number;
+  };
+}
+const AddToBagButton = styled(Button)({
+  backgroundColor: "#b71c1c",
+  color: "#fff",
+  width: "100%",
+  "&:hover": {
+    backgroundColor: "#8e0000",
+  },
+});
+
+const Wishlist: React.FC = () => {
+  const [loading, setLoading] = useState<boolean>(true);
+
+  const [wish, setWish] = useState<Book[]>([]);
+
+  const getWish = async () => {
+    let res = await getWishListService();
+    setLoading(false);
+    console.log(res?.data?.wishlist?.book);
+    setWish(res?.data?.wishlist?.book);
+  };
+  useEffect(() => {
+    getWish();
+  }, []);
+
+  const removeWishList = async (id: string) => {
+    await removeWislListService(id);
+    getWish();
+    // console.log(res?.data);
+  };
+
+  return (
+    <>
+      <div className="cart-container">
+        <h4 style={{ color: "grey" }}>
+          Home/
+          <span style={{ color: "black", fontSize: "15px" }}> (Wishlist)</span>
+        </h4>
+        {loading ? (
+          <Stack spacing={2}>
+            <Stack direction="row" spacing={0}>
+              <Button variant="outlined" disabled>
+                <CircularProgress size={10} />
+              </Button>
+            </Stack>
+          </Stack>
+        ) : wish.length === 0 ? (
+          <p>Your cart is empty</p>
+        ) : (
+          wish.map((ele) => (
+            <div key={ele._id} className="cart-item">
+              <img src={bookImmg} alt={ele.bookName} className="book-image" />
+              <div className="cart-details">
+                <h3>Book Name: {ele.bookname}</h3>
+                <p>Author: {ele.authorname}</p>
+              </div>
+              <Grid item xs={6}>
+                <AddToBagButton
+                  variant="contained"
+                  onClick={() => {
+                    removeWishList(ele.bookId);
+                  }}
+                >
+                  {" "}
+                  Remove WISHLIST
+                </AddToBagButton>
+              </Grid>
+            </div>
+          ))
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Wishlist;
