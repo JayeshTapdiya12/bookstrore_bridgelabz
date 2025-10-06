2131cba7f8c8ad01d6c2132dd4b433a1c5c59fc5
JAYESHTAPDIYA12 <jayesh121220@gmail.com>
2025-10-06 09:42:43 +0530

    [jayesh tapdiya] updated the book service file for the onrender endpoinnt

--- bookstore/src/service/bookSerivce.ts
+++ bookstore/src/service/bookSerivce.ts
@@ -1,13 +1,15 @@
 import axios, { type AxiosResponse } from "axios";
 
-const baseUrl = "http://localhost:3001/api/v1/books";
+// const baseUrl = "http://localhost:3001/api/v1/books";
+const baseUrl = "https://booksbooking.onrender.com/api/v1/books";
+
 const token = localStorage.getItem("token");
 const headers = { headers: { Authorization: "bearer " + token } };
 
 interface bookResp {
   success: boolean;
   message: string;
-  result: {
+  note: {
     bookName: string;
 
     author: string;
@@ -23,15 +25,35 @@ interface bookResp {
   };
 }
 
-export const getBook = async (): Promise<AxiosResponse<bookResp>> => {
+export const getBook = async (): Promise<AxiosResponse<bookRespById>> => {
   let res = await axios.get(baseUrl, headers);
   console.log(res);
   return res;
 };
+export interface Note {
+  _id: string;
+  description: string;
+  discountPrice: number;
+  bookImage: null | string;
+  admin_user_id: string;
+  bookName: string;
+  author: string;
+  quantity: number;
+  price: number;
+  createdAt: string;
+  updatedAt: string;
+  __v: number;
+}
+
+interface bookRespById {
+  code: number;
+  note: Note;
+  message: string;
+}
 
 export const getBookById = async (
   id: string
-): Promise<AxiosResponse<bookResp>> => {
+): Promise<AxiosResponse<bookRespById>> => {
   let res = await axios.get(`${baseUrl}/${id}`, headers);
   console.log(res);
   return res;
