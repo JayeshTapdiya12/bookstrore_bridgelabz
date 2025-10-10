85513be7d5f74bb29872d4637d5d8cedaa0eda60
JAYESHTAPDIYA12 <jayesh121220@gmail.com>
2025-10-10 19:27:40 +0530

    [jayesh tapdiya] updated the service file of wishlist for adding the token and header in the function

--- bookstore/src/service/wishlistService.ts
+++ bookstore/src/service/wishlistService.ts
@@ -3,8 +3,8 @@ import axios, { type AxiosResponse } from "axios";
 // const baseUrl = "http://localhost:3001/api/v1/books/wishlist";
 const baseUrl = "https://booksbooking.onrender.com/api/v1/books/wishlist";
 
-const token = localStorage.getItem("token");
-const headers = { headers: { Authorization: "bearer " + token } };
+// const token = localStorage.getItem("token");
+// const headers = { headers: { Authorization: "bearer " + token } };
 
 export interface GetWishList {
   message: string;
@@ -27,6 +27,8 @@ export interface GetWishList {
 }
 
 export const getWishList = async (): Promise<AxiosResponse<GetWishList>> => {
+  const token = localStorage.getItem("token");
+  const headers = { headers: { Authorization: "bearer " + token } };
   let res = await axios.get(`${baseUrl}/get`, headers);
   return res;
 };
@@ -57,6 +59,8 @@ interface AddWishlist {
 export const addWishlist = async (
   id: string
 ): Promise<AxiosResponse<AddWishlist>> => {
+  const token = localStorage.getItem("token");
+  const headers = { headers: { Authorization: "bearer " + token } };
   let res = await axios.post(`${baseUrl}/add/${id}`, {}, headers);
   return res;
 };
@@ -85,6 +89,8 @@ interface RemoveWishlist {
 export const removeWishlist = async (
   id: string
 ): Promise<AxiosResponse<RemoveWishlist>> => {
+  const token = localStorage.getItem("token");
+  const headers = { headers: { Authorization: "bearer " + token } };
   let res = await axios.post(`${baseUrl}/remove/${id}`, {}, headers);
   return res;
 };
