82b28b3dd5d9615b588b684e243346c27ca1b775
JAYESHTAPDIYA12 <jayesh121220@gmail.com>
2025-10-10 19:27:21 +0530

    [jayesh tapdiya] updated the service file of order for adding the token and header in the function

--- bookstore/src/service/orderService.ts
+++ bookstore/src/service/orderService.ts
@@ -3,8 +3,8 @@ import axios, { type AxiosResponse } from "axios";
 // const baseUrl = "http://localhost:3001/api/v1/order";
 const baseUrl = "https://booksbooking.onrender.com/api/v1/order";
 
-const token = localStorage.getItem("token");
-const headers = { headers: { Authorization: "bearer " + token } };
+// const token = localStorage.getItem("token");
+// const headers = { headers: { Authorization: "bearer " + token } };
 
 interface AddOrder {
   message: string;
@@ -34,7 +34,44 @@ interface AddOrder {
 }
 
 export const addOrder = async (): Promise<AxiosResponse<AddOrder>> => {
+  const token = localStorage.getItem("token");
+  const headers = { headers: { Authorization: "bearer " + token } };
   const res = await axios.post(`${baseUrl}/addorder`, {}, headers);
   localStorage.setItem("orderId", res?.data?.order?.orderId);
+  return res;
+};
+
+interface GetOrderInterface {
+  message: string;
+
+  orders: {
+    isPaid: boolean;
+    isDelivered: boolean;
+    _id: string;
+    orderBy: string;
+    books: [
+      {
+        quantity: number;
+        _id: string;
+        bookId: string;
+        bookName: string;
+        author: string;
+        price: number;
+        discountPrice: number;
+        totalPrice: number;
+      }
+    ];
+    orderTotal: number;
+    orderId: string;
+    createdAt: string;
+    updatedAt: string;
+    __v: number;
+  };
+}
+
+export const getOrder = async (): Promise<AxiosResponse<GetOrderInterface>> => {
+  const token = localStorage.getItem("token");
+  const headers = { headers: { Authorization: "bearer " + token } };
+  const res = await axios.get(`${baseUrl}/allorder`, headers);
   return res;
 };
